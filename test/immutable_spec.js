ed85689a8b8a06efcd448cc67fb026f426da2a7f
Michael Liang <michaelpliang@gmail.com>
2016-03-11 00:05:49 -0800

    next test failing

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,29 +1,35 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
 
   // ...
 
-  describe('A List', () => {
+  describe('a tree', () => {
 
     function addMovie(currentState, movie) {
-      return currentState.push(movie);
+      return currentState.update('movies', movies => movies.push(movie));
     }
 
     it('is immutable', () => {
-      let state = List.of('Trainspotting', '28 Days Later');
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
       let nextState = addMovie(state, 'Sunshine');
 
-      expect(nextState).to.equal(List.of(
-        'Trainspotting',
-        '28 Days Later',
-        'Sunshine'
-      ));
-      expect(state).to.equal(List.of(
-        'Trainspotting',
-        '28 Days Later'
-      ));
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
 
   });
