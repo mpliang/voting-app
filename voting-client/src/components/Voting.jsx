8359307e7be06876a6149c6713a6a64ea2218908
Michael Liang <michaelpliang@gmail.com>
2016-03-14 19:25:42 -0700

    more tests added

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -4,13 +4,21 @@ export default React.createClass({
   getPair: function () {
     return this.props.pair || [];
   },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
   render: function () {
     return (
       <div className="voting">
+        <div ref="winnder">Winner is {this.props.winner}</div> :
         {this.getPair().map(entry => {
           return (
-            <button key={entry}>
+            <button key={entry} disabled={this.isDisabled()} onclick={() => this.props.vote(entry)}>
               <h1>{entry}</h1>
+              {this.hasVotedFor(entry) ? <div className="label">Voted</div> : null}
             </button>
           );
         })}
