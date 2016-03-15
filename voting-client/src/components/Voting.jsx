41c4efbdd3a8031ec3715be8d0c3b81f095f45af
Michael Liang <michaelpliang@gmail.com>
2016-03-14 19:32:02 -0700

    added vote and winner components

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-  getPair: function () {
-    return this.props.pair || [];
-  },
-  isDisabled: function() {
-    return !!this.props.hasVoted;
-  },
-  hasVotedFor: function(entry) {
-    return this.props.hasVoted === entry;
-  },
   render: function () {
     return (
-      <div className="voting">
-        <div ref="winnder">Winner is {this.props.winner}</div> :
-        {this.getPair().map(entry => {
-          return (
-            <button key={entry} disabled={this.isDisabled()} onclick={() => this.props.vote(entry)}>
-              <h1>{entry}</h1>
-              {this.hasVotedFor(entry) ? <div className="label">Voted</div> : null}
-            </button>
-          );
-        })}
+      <div>
+        {this.props.winner
+          ? <Winner ref="winner" winner={this.props.winner}/>
+          : <Vote {...this.props}/>}
       </div>
     );
   }
+  
 });
